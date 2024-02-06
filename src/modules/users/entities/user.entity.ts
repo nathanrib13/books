504897dc68b564ec3b28ac8d5c07c455f0c3bfe8
nathanrib13 <nathan.ribeiros7@gmail.com>
2024-02-06 20:56:27 -0300

    create user

--- src/modules/users/entities/user.entity.ts
+++ src/modules/users/entities/user.entity.ts
@@ -1,2 +1,13 @@
 /* eslint-disable prettier/prettier */
-export class User {}
+import { randomUUID } from "crypto"
+
+export class User {
+    readonly id: string
+    name: string
+    email: string
+    password: string
+
+    constructor(){
+        this.id = randomUUID()
+    }
+}
