409af1c6852a2824a36f2ad681a2e9a01a34145b
nathanrib13 <nathan.ribeiros7@gmail.com>
2024-02-16 20:05:01 -0300

    adding hash password

--- src/modules/users/dto/create-user.dto.ts
+++ src/modules/users/dto/create-user.dto.ts
@@ -1,6 +1,23 @@
 /* eslint-disable prettier/prettier */
+
+import { hashSync } from "bcryptjs";
+import { Transform } from "class-transformer";
+import { IsEmail, IsNotEmpty, IsString, MinLength } from "class-validator";
+
 export class CreateUserDto {
+    @IsString()
+    @IsNotEmpty()
     name: string;
-    email: string; 
+    
+    @IsEmail()
+    @IsNotEmpty()
+    email: string;
+
+    @IsString()
+    @IsNotEmpty() 
+    @MinLength(8)
+    @Transform(({value}: {value:string}) => hashSync(value, 10), {
+        groups:['transform']
+    })
     password: string;
 }
